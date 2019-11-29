84666ee0016a64f633cf72a5ed45da6d9d96c091
laszlosimity <lsimity@gmail.com>
2019-11-29 11:40:21 +0000

    Migrated to Nightwatch 1, and moved to new node-saucelabs service

--- custom_commands/customSauceEnd.js
+++ custom_commands/customSauceEnd.js
@@ -1,24 +1,19 @@
 exports.command = function(callback) {
-    var SauceLabs = require("saucelabs");
+    const SauceLabs = require("saucelabs");
 
-    var saucelabs = new SauceLabs({
-        username: process.env.SAUCE_USERNAME,
-        password: process.env.SAUCE_ACCESS_KEY
+    const myAccount = new SauceLabs.default({
+        user: process.env.SAUCE_USERNAME,
+        key: process.env.SAUCE_ACCESS_KEY,
+        region: 'eu'
     });
 
     var sessionid = this.capabilities['webdriver.remote.sessionid'],
         jobName = this.currentTest.name,
         passed = this.currentTest.results.testcases[jobName].failed === 0;
 
-
     console.log("SauceOnDemandSessionID=" + sessionid + " job-name=" + jobName);
 
-	var self = this
-	saucelabs.updateJob(sessionid, {
-    passed: passed,
-    name: jobName
-	}, function () {
-    	self.end(callback)
-	});
+    myAccount.updateJob(process.env.SAUCE_USERNAME, sessionid, {passed: passed, name: 'testnamehere' });
 
 };
+
